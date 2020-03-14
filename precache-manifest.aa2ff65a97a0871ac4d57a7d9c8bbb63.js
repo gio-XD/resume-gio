01b008f94fc48a114ad94c10d421b893a83675b8
Gio <1193078960@qq.com>
2020-03-14 17:21:54 +0800

    Updates

---
+++ precache-manifest.aa2ff65a97a0871ac4d57a7d9c8bbb63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4d85a62a05c0dd0525f39126b958928",
+    "url": "./index.html"
+  },
+  {
+    "revision": "13e4ea6e60bb0ed19da2",
+    "url": "./static/css/main.a4d9404d.chunk.css"
+  },
+  {
+    "revision": "894a1fdf85623dd9d7bb",
+    "url": "./static/js/2.7b0b4109.chunk.js"
+  },
+  {
+    "revision": "11481c222843c5704bd0e823ad6f6998",
+    "url": "./static/js/2.7b0b4109.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13e4ea6e60bb0ed19da2",
+    "url": "./static/js/main.27ffbe9f.chunk.js"
+  },
+  {
+    "revision": "bfcd80363613bedd3b26",
+    "url": "./static/js/runtime-main.52066a21.js"
+  }
+]);
